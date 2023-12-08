934997cfdbf27c7dba468c87cd4a0fc9638cd420
Katsya-Yoneno <hjkshn0405@gmail.com>
2023-12-08 17:21:10 +0900

    2023-12-08 17:21:10

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,108 @@
-import './App.css';
+import { useCallback } from 'react'
+import Particles from 'react-particles'
+import { loadSlim } from 'tsparticles-slim'
+import Bio from './Bio'
+import TimeLine from './TimeLine'
+import './App.css'
 
-function App() {
-  return (
-    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello Tailwind CSS!
-      </h1>
-      <p className="m-0 text-gray-400">Tailwind CSSです</p>
-      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">ボタン</button>
-    </div>
-  );
+const App = () => {
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine)
+        await loadSlim(engine)
+    }, [])
+
+    const particlesLoaded = useCallback(async (container) => {
+        console.log(container)
+    }, [])
+
+    return (
+        <>
+            <div className="relative">
+                <Particles
+                    id="tsparticles"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                    options={{
+                        background: {
+                            color: {
+                                value: '#171215',
+                            },
+                        },
+                        fpsLimit: 120,
+                        interactivity: {
+                            events: {
+                                onClick: {
+                                    enable: true,
+                                    mode: 'push',
+                                },
+                                onHover: {
+                                    enable: true,
+                                    mode: 'repulse',
+                                },
+                                resize: true,
+                            },
+                            modes: {
+                                push: {
+                                    quantity: 4,
+                                },
+                                repulse: {
+                                    distance: 200,
+                                    duration: 0.4,
+                                },
+                            },
+                        },
+                        particles: {
+                            color: {
+                                value: '#ffffff',
+                            },
+                            links: {
+                                color: '#ffffff',
+                                distance: 150,
+                                enable: true,
+                                opacity: 0.5,
+                                width: 1,
+                            },
+                            move: {
+                                direction: 'none',
+                                enable: true,
+                                outModes: {
+                                    default: 'bounce',
+                                },
+                                random: false,
+                                speed: 5,
+                                straight: false,
+                            },
+                            number: {
+                                density: {
+                                    enable: true,
+                                    area: 1500,
+                                },
+                                value: 100,
+                            },
+                            opacity: {
+                                value: 0.7,
+                            },
+                            shape: {
+                                type: 'circle',
+                            },
+                            size: {
+                                value: { min: 1, max: 5 },
+                            },
+                        },
+                        detectRetina: true,
+                    }}
+                />
+            </div>
+            <div className="flex justify-center">
+                <div className="flex-none md:mt-20 md:mr-20 md:w-96 sm:w-80 w-64">
+                    <Bio />
+                </div>
+                <div className="flex-none md:mt-20 md:mr-20 md:w-80 sm:w-96 w-64">
+                    <TimeLine />
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default App;
-
+export default App
---
+++ src/TimeLine.jsx
@@ -0,0 +1,174 @@
+import './App.css'
+
+const TimeLine = () => {
+    return (
+        <>
+            <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:ml-[8.75rem] md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 ">
+                <div className="relative">
+                    <div className="md:flex items-center md:space-x-4 mb-3">
+                        <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
+                            {/* icon */}
+                            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
+                                <svg
+                                    className="fill-emerald-500"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                >
+                                    <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
+                                </svg>
+                                {/* date */}
+                            </div>
+                            <time className="font-caveat font-medium text-xl text-white md:w-28">Apr 4, 1999</time>
+                        </div>
+                        {/* title */}
+                        <div className="text-slate-200 ml-14">
+                            <span className="text-green-200 font-bold font-medium text-xl">Hello World</span>
+                        </div>
+                        {/* card */}
+                    </div>
+                    <div className="w-96 bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-44">
+                        爆誕しました
+                    </div>
+                </div>
+                <div className="relative">
+                    <div className="md:flex items-center md:space-x-4 mb-3">
+                        <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
+                            {/* icon */}
+                            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
+                                <svg
+                                    className="fill-emerald-500"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                >
+                                    <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
+                                </svg>
+                                {/* date */}
+                            </div>
+                            <time className="font-caveat font-medium text-xl text-white md:w-28">Apr 4, 1999</time>
+                        </div>
+                        {/* title */}
+                        <div className="text-slate-200 ml-14">
+                            <span className="text-green-200 font-bold font-medium text-xl">Hello World</span>
+                        </div>
+                        {/* card */}
+                    </div>
+                    <div className="w-96 bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-44">
+                        爆誕しました
+                    </div>
+                </div>
+                <div className="relative">
+                    <div className="md:flex items-center md:space-x-4 mb-3">
+                        <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
+                            {/* icon */}
+                            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
+                                <svg
+                                    className="fill-emerald-500"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                >
+                                    <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
+                                </svg>
+                                {/* date */}
+                            </div>
+                            <time className="font-caveat font-medium text-xl text-white md:w-28">Apr 4, 1999</time>
+                        </div>
+                        {/* title */}
+                        <div className="text-slate-200 ml-14">
+                            <span className="text-green-200 font-bold font-medium text-xl">Hello World</span>
+                        </div>
+                        {/* card */}
+                    </div>
+                    <div className="w-96 bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-44">
+                        爆誕しました
+                    </div>
+                </div>
+                <div className="relative">
+                    <div className="md:flex items-center md:space-x-4 mb-3">
+                        <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
+                            {/* icon */}
+                            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
+                                <svg
+                                    className="fill-emerald-500"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                >
+                                    <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
+                                </svg>
+                                {/* date */}
+                            </div>
+                            <time className="font-caveat font-medium text-xl text-white md:w-28">Apr 4, 1999</time>
+                        </div>
+                        {/* title */}
+                        <div className="text-slate-200 ml-14">
+                            <span className="text-green-200 font-bold font-medium text-xl">Hello World</span>
+                        </div>
+                        {/* card */}
+                    </div>
+                    <div className="w-96 bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-44">
+                        爆誕しました
+                    </div>
+                </div>
+                <div className="relative">
+                    <div className="md:flex items-center md:space-x-4 mb-3">
+                        <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
+                            {/* icon */}
+                            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
+                                <svg
+                                    className="fill-emerald-500"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                >
+                                    <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
+                                </svg>
+                                {/* date */}
+                            </div>
+                            <time className="font-caveat font-medium text-xl text-white md:w-28">Apr 4, 1999</time>
+                        </div>
+                        {/* title */}
+                        <div className="text-slate-200 ml-14">
+                            <span className="text-green-200 font-bold font-medium text-xl">Hello World</span>
+                        </div>
+                        {/* card */}
+                    </div>
+                    <div className="w-96 bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-44">
+                        爆誕しました
+                    </div>
+                </div>
+                <div className="relative">
+                    <div className="md:flex items-center md:space-x-4 mb-3">
+                        <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
+                            {/* icon */}
+                            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
+                                <svg
+                                    className="fill-emerald-500"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                >
+                                    <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
+                                </svg>
+                                {/* date */}
+                            </div>
+                            <time className="font-caveat font-medium text-xl text-white md:w-28">Apr 4, 1999</time>
+                        </div>
+                        {/* title */}
+                        <div className="text-slate-200 ml-14">
+                            <span className="text-green-200 font-bold font-medium text-xl">Hello World</span>
+                        </div>
+                        {/* card */}
+                    </div>
+                    <div className="w-96 bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-44">
+                        爆誕しました
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default TimeLine
